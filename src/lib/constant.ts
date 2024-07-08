c727167cd65846992c3b27d315e9a2e94fc7e5fa
vuvlnvw <vuvlnvw@gmail.com>
2024-07-08 03:27:32 +0000

    [add] constant

---
+++ src/lib/constant.ts
@@ -0,0 +1,237 @@
+interface Province {
+	name: string;
+	capital: string;
+	logo: string;
+	inaugurationDate: string;
+}
+
+export const provinces: Province[] = [
+	{
+		name: 'Nanggroe Aceh Darussalam',
+		capital: 'Banda Aceh',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Sumatera Utara',
+		capital: 'Medan',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Sumatera Selatan',
+		capital: 'Palembang',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Sumatera Barat',
+		capital: 'Padang',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Bengkulu',
+		capital: 'Bengkulu',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Riau',
+		capital: 'Pekanbaru',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Kepulauan Riau',
+		capital: 'Tanjung Pinang',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Jambi',
+		capital: 'Jambi',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Lampung',
+		capital: 'Bandar Lampung',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Bangka Belitung',
+		capital: 'Pangkal Pinang',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Kalimantan Barat',
+		capital: 'Pontianak',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Kalimantan Timur',
+		capital: 'Samarinda',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Kalimantan Selatan',
+		capital: 'Banjarbaru',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Kalimantan Tengah',
+		capital: 'Palangkaraya',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Kalimantan Utara',
+		capital: 'Tanjung Selor',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Banten',
+		capital: 'Serang',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'DKI Jakarta',
+		capital: 'Jakarta',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Jawa Barat',
+		capital: 'Bandung',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Jawa Tengah',
+		capital: 'Semarang',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Daerah Istimewa Yogyakarta',
+		capital: 'Yogyakarta',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Jawa Timur',
+		capital: 'Surabaya',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Bali',
+		capital: 'Denpasar',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Nusa Tenggara Timur',
+		capital: 'Kupang',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Nusa Tenggara Barat',
+		capital: 'Mataram',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Gorontalo',
+		capital: 'Gorontalo',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Sulawesi Barat',
+		capital: 'Mamuju',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Sulawesi Tengah',
+		capital: 'Palu',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Sulawesi Utara',
+		capital: 'Manado',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Sulawesi Tenggara',
+		capital: 'Kendari',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Sulawesi Selatan',
+		capital: 'Makassar',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Maluku Utara',
+		capital: 'Sofifi',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Maluku',
+		capital: 'Ambon',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Papua Barat',
+		capital: 'Manokwari',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Papua',
+		capital: 'Jayapuri',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Papua Tengah',
+		capital: 'Nabire',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Papua Pegunungan',
+		capital: 'Jayawijaya',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Papua Selatan',
+		capital: 'Merauke',
+		logo: '',
+		inaugurationDate: ''
+	},
+	{
+		name: 'Papua Barat Daya',
+		capital: 'Sorong',
+		logo: '',
+		inaugurationDate: ''
+	}
+];
